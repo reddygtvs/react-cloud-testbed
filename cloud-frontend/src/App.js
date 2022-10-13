d56aa14b8ffc7c1ea67a87db8e045d86ef605e40
Tushar <reddygtvs@gmail.com>
2022-10-13 18:07:22 +0530

    minor changes to the deploy command

--- cloud-frontend/src/App.js
+++ cloud-frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hosting on <code>S3 Bucket</code> to check if it works
         </p>
         <a
           className="App-link"
